5bc80f384bd16bb561385266e2d38497c53a8154
Griffin Poole <griffinpoole5@gmail.com>
2019-11-02 21:15:34 -0400

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,10 +1,38 @@
 class Mechanic
 
+  @@all = []
   attr_reader :name, :specialty
 
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  @@all << self
+end
+
+#gets list of all mechanics
+def self.all
+  @@all
+end
+
+#gets all cars this mechanic instance services
+def cars
+  Car.all.select{|car|
+    car.mechanic == self
+  }
+end
+
+#lists all car owners that go to this mechanic
+def owners
+  cars.map{|car|
+    car.owner
+  }
+end
+
+#gets names of all owners who visit this mechanic
+def owner_names
+  owners.map{|owner|
+    owner.name
+  }
 end
 
 

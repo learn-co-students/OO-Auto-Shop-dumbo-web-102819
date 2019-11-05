d01352d261ba1fa16e6629672c0844a3ec540806
ozubergz <jmin499@gmail.com>
2019-11-05 17:40:47 -0500

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,37 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
 
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select do |instance|
+      instance.mechanic == self
+    end
+  end
+
+  def car_owners
+    cars.map do |instance|
+      instance.owner
+    end
+  end
+
+  def car_owner_names
+    car_owners.map do |instance|
+      instance.name
+    end
+  end
+
+  
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,22 @@ def reload
   load 'config/environment.rb'
 end
 
+# model_types = ["sedan", "coupe", "wagon", "convertible", "wagon"]
+#car_classifications = ["antique", "exotic", "old", "supercar"]
+
+#car owner instance
+jay = CarOwner.new("Jay")
+stan = CarOwner.new("Stan")
+
+#machanic instance
+bob = Mechanic.new("Bob", "supercar")
+bill = Mechanic.new("Bill", "old")
+will = Mechanic.new("Will", "exotic")
+
+#car instance
+lambo = Car.new("Lambo", "sedan", "supercar", jay, bob)
+bmw = Car.new("BMW", "coupe", "old", jay, bill)
+bugatti = Car.new("Bugatti", "convertible", "supercar", stan, will)
+ford = Car.new("Ford", "wagon", "exotic", stan, will)
+
 binding.pry
